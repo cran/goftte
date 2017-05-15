3341a8d31ee6db3ef5e1b7ec1d26a279cfcbdaef
Patrick Sfumato <sfumatop@ipc.unicancer.fr>
2017-05-15 08:42:57 +0000

    version 1.0

---
+++ R/predband.scproc.R
@@ -0,0 +1,15 @@
+##' @export
+`predband` <-
+  function(x,...) UseMethod("predband")
+
+predband.scproc <- function(x, idx=1:length(x$variable), level=0.95, cval=NULL, ...) {
+  t <- c(); yu <- c()
+  for (i in idx) {
+    if (is.null(cval))
+      cval <- quantile(x$cvalues[,,i], level)
+    ##  y <- x$W[,i];
+    t <- cbind(t,x$obs[,,i])
+    yu <- cbind(yu,cval*x$sd[,,i]);
+  }
+  return(list(t=t,yu=yu));
+}
